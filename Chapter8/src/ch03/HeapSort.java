30e828f2e8fb956b3b07e4c824272025fa83b9a6
easyspubjava <eunjong.park@gmail.com>
2021-04-20 00:17:44 +0900

    sort

---
+++ Chapter8/src/ch03/HeapSort.java
@@ -0,0 +1,80 @@
+package ch03;
+
+public class HeapSort {
+
+	private int SIZE;
+	private int heapArr[];
+	
+	public HeapSort()
+	{
+		SIZE = 0;
+		heapArr = new int [50];
+	}
+	
+	public void insertHeap(int input)
+	{
+		int i = ++SIZE;
+		//while(( i != 1) && ( input > heapArr[i/2])){ //max heap
+		while(( i != 1) && ( input < heapArr[i/2])){ //min heap
+			heapArr[i] = heapArr[i/2];
+			i = i/2;
+		}
+		heapArr[i] = input;
+	}
+	
+	public int getHeapSize()
+	{
+		return SIZE;
+	}
+	
+	public int deleteHeap()
+	{
+		int parent, child;
+		int data, temp;
+		data = heapArr[1]; 
+		
+		temp = heapArr[SIZE]; 
+		SIZE -= 1; 
+		parent =1; child = 2;
+		
+		while(child <= SIZE){
+			//if((child < HEAP_SIZE) && (heapArr[child] < heapArr[child+1])){ //max heap
+			if((child < SIZE) && (heapArr[child] > heapArr[child+1])){ //min heap
+				child++;
+			}
+			//if(temp >= heapArr[child]) break; //max heap
+			if(temp <= heapArr[child]) break;   //min heap
+			heapArr[parent] = heapArr[child];
+			parent = child;
+			child *= 2;
+		}
+		
+		heapArr[parent] = temp;
+		return data;
+	}
+	
+	public void printHeap(){
+		//System.out.printf("\n Max Heap : ");
+		System.out.printf("\n Min Heap : ");
+		for(int i=1; i<=SIZE; i++)
+			System.out.printf("[%d] ", heapArr[i]);
+	}
+	public static void main(String[] args) {
+		HeapSort h = new HeapSort();
+		h.insertHeap(80);
+		h.insertHeap(50);
+		h.insertHeap(70);
+		h.insertHeap(10);
+		h.insertHeap(60);
+		h.insertHeap(20);
+		
+		h.printHeap();
+	
+		int n, data;
+		n = h.getHeapSize();
+		for(int i=1; i<=n; i++){    
+			data = h.deleteHeap();  
+			System.out.printf("\n Ãâ·Â : [%d]", data);
+		}
+	}
+}
---
+++ Chapter8/src/ch03/InsertionSort.java
@@ -0,0 +1,41 @@
+package ch03;
+
+public class InsertionSort {
+
+	public static void insertionSort(int[] arr, int count) {
+		
+		int i = 0, j = 0;
+		int temp = 0;
+
+		for(i = 1; i < count; i++) {
+			temp = arr[i];
+			j = i;
+			while((j > 0) && arr[j-1] > temp) {
+				arr[j] = arr[j-1];
+				j = j - 1;
+			}
+			arr[j] = temp;
+
+			System.out.println("นÝบน -" + i);
+			printSort(arr, count);
+		}
+		
+	}
+	
+	public static void printSort(int value[], int count)
+	{
+		int i = 0;
+		for(i = 0; i < count; i++) {
+			System.out.print(value[i] + "\t");
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] arr = {80, 50, 70, 10, 60, 20, 40, 30 };
+		
+		insertionSort(arr, 8);
+	}
+
+}
